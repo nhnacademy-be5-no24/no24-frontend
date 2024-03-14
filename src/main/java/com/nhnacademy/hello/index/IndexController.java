147e8088620149e83dc6a170b8efc61955bdfce3
BHwi <bhwi0422@gmail.com>
2024-03-14 14:45:37 +0900

    example commit

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -2,11 +2,20 @@ package com.nhnacademy.hello.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Controller
 public class IndexController {
     @GetMapping(value = {"/index.html","/"})
-    public String index(){
-        return "index/index";
+    public ModelAndView index() throws UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
+
+        ModelAndView mav = new ModelAndView("index/index");
+        mav.addObject("ip", localHost.getHostAddress());
+
+        return mav;
     }
 }
