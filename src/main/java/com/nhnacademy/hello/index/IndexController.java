9ae41724a9e489435dd59b1617ce1f8f97ab9a5d
BHwi <43560497+BHwi@users.noreply.github.com>
2024-03-15 16:22:23 +0900

    Update IndexController.java

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -4,17 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @Controller
 public class IndexController {
     @GetMapping(value = {"/index.html","/"})
-    public ModelAndView index() throws UnknownHostException {
+    public ModelAndView index(HttpServletRequest request) throws UnknownHostException {
         InetAddress localHost = InetAddress.getLocalHost();
 
         ModelAndView mav = new ModelAndView("index/index");
         mav.addObject("ip", localHost.getHostAddress());
+        mav.addObject("port", request.getServerPort());
 
         return mav;
     }
