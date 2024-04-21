2b62458cb4430217e30b74bb6770829fb3c5d1e1
jinjiwon <jinjw5441@gmail.com>
2024-04-21 18:37:25 +0900

    fix: json 직렬화 및 역직렬화 설정

--- src/main/java/com/nhnacademy/frontend/config/RedisConfig.java
+++ src/main/java/com/nhnacademy/frontend/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -44,8 +45,11 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         // redis client에서 데이터 확인 시 알 수 없는 형태로 출력 되는 것을 방지
+        // JSON 직렬화 및 역직렬화 설정
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
     }
---
+++ src/main/java/com/nhnacademy/frontend/main/cart/dto/response/CartResponseDto.java
@@ -0,0 +1,40 @@
+package com.nhnacademy.shop.cart.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartResponseDto {
+    @JsonProperty("cart_id")
+    private Long cartId;
+
+    @JsonProperty("book_isbn")
+    private String bookIsbn;
+
+    @JsonProperty("book_title")
+    private String bookTitle;
+
+    @JsonProperty("book_fixed_price")
+    private int bookFixedPrice;
+
+    @JsonProperty("book_sale_price")
+    private int bookSalePrice;
+
+    @JsonProperty("book_status")
+    private int bookStatus;
+
+//    @JsonProperty("book_quantity")
+//    private Long bookQuantity;
+
+    @JsonProperty("book_image")
+    private String bookImage;
+
+    @JsonProperty("customer_no")
+    private Long customerNo;
+}
