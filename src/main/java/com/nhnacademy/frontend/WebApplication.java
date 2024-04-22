3ab85c4e1b54c5889ac8f3b496b615f216e30f76
KIM BYEONG JU <100591433+hagisilm@users.noreply.github.com>
2024-04-22 18:10:32 +0900

    feat: 회원가입,로그인 설정

--- src/main/java/com/nhnacademy/frontend/WebApplication.java
+++ src/main/java/com/nhnacademy/frontend/WebApplication.java
@@ -2,6 +2,9 @@ package com.nhnacademy.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WebApplication {
@@ -10,4 +13,8 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
 }
--- src/main/java/com/nhnacademy/frontend/auth/LoginController.java
+++ src/main/java/com/nhnacademy/frontend/auth/LoginController.java
@@ -1,7 +1,13 @@
 package com.nhnacademy.frontend.auth;
 
+import com.nhnacademy.frontend.auth.dto.MemberCreateRequest;
+import com.nhnacademy.frontend.auth.dto.LoginDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -12,10 +18,41 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class LoginController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/login")
     public ModelAndView getLoginPage() {
         ModelAndView mav = new ModelAndView("index/auth/login");
 
         return mav;
+    }
+
+    @GetMapping("/registerPage")
+    public ModelAndView getRegisterPage() {
+        ModelAndView mav = new ModelAndView("index/auth/register");
+        return mav;
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginDto loginDto) {
+        System.out.println(loginDto.toString());
+        restTemplate.postForEntity(
+                "http://localhost:8081/auth/member/login",
+                loginDto,
+                LoginDto.class
+        );
+        return "index/auth/loginSuccess";
+    }
+
+    @PostMapping("/register")
+    public String createMember(@ModelAttribute MemberCreateRequest memberCreateRequest) {
+        restTemplate.postForEntity(
+                "http://localhost:8081/auth/member/create",
+                memberCreateRequest,
+                MemberCreateRequest.class
+        );
+        return "index/auth/registerSuccess";
     }
 }
