9ea7de5253ba4fd1d474bde42b325bce24eab476
jinjiwon <jinjw5441@gmail.com>
2024-04-22 23:15:25 +0900

    feat: 결제 완료 후, 장바구니 삭제 구현(장바구니 전체 삭제/수정 필요

--- src/main/java/com/nhnacademy/frontend/main/cart/controller/CartController.java
+++ src/main/java/com/nhnacademy/frontend/main/cart/controller/CartController.java
@@ -102,4 +102,27 @@ public class CartController {
             throw new CartNotFoundException(customerId);
         }
     }
+
+    // todo: 장바구니 삭제
+    // todo-1: 삭제 버튼 클릭 시, 장바구니 삭제 => js로
+    // todo-2: 상품 결제 후, 장바구니 삭제 => 여기서 구현 => 구현 완료
+    // todo-3 : todo-2 경우, 장바구니 상품 중 일부 구매하는 경우 고려해서 구현해야 함 !!!!!!
+    /**
+     * [DELETE /cart/delete/{customerId}]
+     * 결제 완료 후, customerId의 장바구니를 삭제하는 DELETE 메서드
+     */
+    @DeleteMapping("/delete/{customerId}")
+    public ResponseEntity<String> deleteCart(@PathVariable String customerId, @RequestBody List<CartRequestDto> cartRequestDtoList) {
+        HashOperations<String, String, Cart> hashOperations = redisTemplate.opsForHash();
+
+        if (hashOperations.hasKey("cart", customerId)) {
+            hashOperations.delete("cart", customerId);
+            return ResponseEntity.ok(customerId + "의 장바구니가 삭제되었습니다.");
+        } else {
+            throw new CartNotFoundException(customerId);
+        }
+    }
+
+    // todo: 장바구니에서 선택한 상품을 주문 페이지에 어떤 형식으로 넘겨주는지 확인 필요 => delete 구현
+    //  이렇게 넘겨지는 형식을 @RequestBody로 받아서 redis에서 구매한 상품만 삭제
 }
