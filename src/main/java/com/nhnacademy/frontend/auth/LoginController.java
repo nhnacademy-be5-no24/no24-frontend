1f25e626ef8884695c61dc70b9f6ddcb5ef8ad48
BHwi <bhwi0422@gmail.com>
2024-04-23 16:11:26 +0900

    add: refresh jwt token

--- src/main/java/com/nhnacademy/frontend/auth/LoginController.java
+++ src/main/java/com/nhnacademy/frontend/auth/LoginController.java
@@ -2,13 +2,29 @@ package com.nhnacademy.frontend.auth;
 
 import com.nhnacademy.frontend.auth.dto.MemberCreateRequest;
 import com.nhnacademy.frontend.auth.dto.LoginDto;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Login Controller
@@ -18,13 +34,31 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class LoginController {
+    @Value("${request.url}")
+    private String requestUrl;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @Value("${request.port}")
+    private String port;
+
+    private final RestTemplate restTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public LoginController(RestTemplate restTemplate, RedisTemplate<String, Object> redisTemplate) {
+        this.restTemplate = restTemplate;
+        this.redisTemplate = redisTemplate;
+    }
 
     @GetMapping("/login")
-    public ModelAndView getLoginPage() {
+    public ModelAndView getLoginPage(HttpServletRequest httpServletRequest) {
         ModelAndView mav = new ModelAndView("index/auth/login");
+
+        HttpSession session = httpServletRequest.getSession();
+        String jSessionId = session.getId();
+        String value = (String) redisTemplate.opsForHash().get(jSessionId, "Authorization");
+
+        if(value != null) {
+            mav.setViewName("redirect:/");
+        }
 
         return mav;
     }
@@ -36,23 +70,62 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginDto loginDto) {
-        System.out.println(loginDto.toString());
-        restTemplate.postForEntity(
-                "http://localhost:8081/auth/member/login",
-                loginDto,
+    public String login(HttpServletRequest httpServletRequest) {
+        // RestTemplate 객체 생성
+        RestTemplate restTemplate = new RestTemplate();
+        HttpSession session = httpServletRequest.getSession();
+        String jSessionId = session.getId();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // 요청 본문 데이터 설정 (form-data)
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("username", httpServletRequest.getParameter("username"));
+        map.add("password", httpServletRequest.getParameter("password"));
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+        ResponseEntity responseEntity = restTemplate.postForEntity(
+                requestUrl + ":" + port +  "/login",
+                request,
                 LoginDto.class
         );
-        return "index/auth/loginSuccess";
+
+        // header의 authorization와 refreshToken을 얻어온다.
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+        String authorization = responseEntity.getHeaders().get("Authorization").get(0);
+        String refreshToken = responseEntity.getHeaders().get("RefreshToken").get(0);
+
+        // Redis session에 저장.
+        hashOperations.put(jSessionId, "Authorization", authorization);
+        hashOperations.put(jSessionId, "RefreshToken", refreshToken);
+
+        String value = (String) redisTemplate.opsForHash().get(jSessionId, "Authorization");
+
+        return "redirect:/";
     }
 
     @PostMapping("/register")
     public String createMember(@ModelAttribute MemberCreateRequest memberCreateRequest) {
         restTemplate.postForEntity(
-                "http://localhost:8081/auth/member/create",
+                requestUrl + ":" + port + "/auth/member/create",
                 memberCreateRequest,
                 MemberCreateRequest.class
         );
         return "index/auth/registerSuccess";
+    }
+
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest httpServletRequest) {
+        HttpSession session = httpServletRequest.getSession();
+        String jSessionId = session.getId();
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+
+        hashOperations.delete(jSessionId, "Authorization");
+        hashOperations.delete(jSessionId, "RefreshToken");
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/nhnacademy/frontend/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,50 @@
+package com.nhnacademy.frontend.interceptor;
+
+import com.nhnacademy.frontend.auth.dto.LoginDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * 설명
+ *
+ * @Author : 박병휘
+ * @Date : 2024/04/22
+ */
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final RestTemplate restTemplate;
+
+    public LoginCheckInterceptor(RestTemplate restTemplate, RedisTemplate<String, Object> redisTemplate) {
+        this.restTemplate = restTemplate;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        String jSessionId = session.getId();
+        String token = (String) redisTemplate.opsForHash().get(jSessionId, "Authorization");
+        String refreshToken = (String) redisTemplate.opsForHash().get(jSessionId, "RefreshToken");
+
+        return true;
+    }
+
+    public boolean isLoggedIn(String jSessionId) {
+        String token = (String) redisTemplate.opsForHash().get(jSessionId, "Authorization");
+        String refreshToken = (String) redisTemplate.opsForHash().get(jSessionId, "RefreshToken");
+
+        return token != null && refreshToken != null;
+    }
+}
