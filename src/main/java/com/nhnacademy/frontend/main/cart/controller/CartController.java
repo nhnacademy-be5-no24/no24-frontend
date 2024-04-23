2d7031728e6a110e3e006bc1dfe9c69c5b6a0aef
jinjiwon <jinjw5441@gmail.com>
2024-04-23 13:44:23 +0900

    feat: 사용자의 action으로 장바구니 내 '하나'의 상품 삭제 구현

--- src/main/java/com/nhnacademy/frontend/main/cart/controller/CartController.java
+++ src/main/java/com/nhnacademy/frontend/main/cart/controller/CartController.java
@@ -34,7 +34,6 @@ public class CartController {
     private static final ObjectMapper mapper = new ObjectMapper();
 
     // todo 0: customerId는 cookie를 통해서 가져올 예정
-
     /**
      * [POST /cart/create]
      * 장바구니에 상품을 추가하는 Post 메소드
@@ -96,15 +95,30 @@ public class CartController {
             }
 
             hashOperations.put("cart", customerId, cart);
-
             return ResponseEntity.ok("장바구니 상품 수량이 성공적으로 업데이트 되었습니다.");
         } else {
             throw new CartNotFoundException(customerId);
         }
     }
 
-    // todo: 장바구니 삭제
-    // todo-1: 삭제 버튼 클릭 시, 장바구니 삭제 => js로
+    /**
+     * [DELETE /cart/deleteOne/{customerId}]
+     * 사용자의 action으로 장바구니 내 '하나'의 상품 삭제
+     */
+    @DeleteMapping("/deleteOne/{customerId}")
+    public ResponseEntity<String> deleteCartBook(@PathVariable String customerId, @RequestParam String bookIsbn) {
+        HashOperations<String, String, Cart> hashOperations = redisTemplate.opsForHash();
+
+        if (hashOperations.hasKey("cart", customerId)) {
+            Cart cart = hashOperations.get("cart", customerId);
+            cart.getBooks().removeIf(book -> book.getIsbn().equals(bookIsbn));
+            hashOperations.put("cart", customerId, cart);
+            return ResponseEntity.ok(bookIsbn) + "이 장바구니에서 삭제되었습니다.");
+        } else {
+            throw new CartNotFoundException(customerId);
+        }
+    }
+
     // todo-2: 상품 결제 후, 장바구니 삭제 => 여기서 구현 => 구현 완료
     // todo-3 : todo-2 경우, 장바구니 상품 중 일부 구매하는 경우 고려해서 구현해야 함 !!!!!!
     /**
