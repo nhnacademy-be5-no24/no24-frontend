db778bbefdb4a80350ae5f8703daf7041b69807d
byegu <rkdqudrn07@naver.com>
2024-05-07 10:47:21 +0900

    Aspect 구현

---
+++ src/main/java/com/nhnacademy/frontend/main/member/dto/MemberInfoResponseDto.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.frontend.main.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Member 기본 정보를 받기 위한 입니다.
+ *
+ * @author : 강병구
+ * @date : 2024-05-02
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MemberInfoResponseDto {
+    private String memberId;
+    private LocalDateTime lastLoginAt;
+    private String role;
+}
