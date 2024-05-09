7a828ad2b04d98135043e7a77dc4cc0076e019a2
BHwi <bhwi0422@gmail.com>
2024-05-09 17:32:44 +0900

    add: try-catch on birthday

--- src/main/java/com/nhnacademy/frontend/auth/LoginController.java
+++ src/main/java/com/nhnacademy/frontend/auth/LoginController.java
@@ -119,18 +119,17 @@ public class LoginController {
 
             JsonNode member = jsonResponse.get("data").get("member");
             String idNo = member.get("idNo").asText();
-            String email = member.get("email").asText() == null ? "미입력" : member.get("email").asText();
-            String mobile = member.get("mobile").asText() == null ? "미입력" : member.get("mobile").asText();
-            String name = member.get("name").asText() == null ? "미입력" : member.get("name").asText();
+            String email = member.get("email") == null ? "미입력" : member.get("email").asText();
+            String mobile = member.get("mobile") == null ? "미입력" : member.get("mobile").asText();
+            String name = member.get("name") == null ? "미입력" : member.get("name").asText();
             LocalDate birthday = null;
 
-            if(member.get("birthdayMMdd") != null && member.get("ageGroup") != null) {
+            try {
                 int year = LocalDate.now().getYear() - Integer.parseInt(member.get("ageGroup").asText());
                 int month = Integer.parseInt(member.get("birthdayMMdd").asText().substring(0, 2));
                 int day = Integer.parseInt(member.get("birthdayMMdd").asText().substring(2));
                 birthday = LocalDate.of(year, month, day);
-            }
-            else {
+            } catch(Exception e) {
                 birthday = LocalDate.now();
             }
 
