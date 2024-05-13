e9ac7c3bb56c585ddd215a24eb68bf28129b6399
BHwi <bhwi0422@gmail.com>
2024-05-13 10:47:54 +0900

    add: Load Test

--- src/main/java/com/nhnacademy/frontend/loaderio/LoadTestController.java
+++ src/main/java/com/nhnacademy/frontend/loaderio/LoadTestController.java
@@ -2,6 +2,7 @@ package com.nhnacademy.frontend.loaderio;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 부하 테스트 컨트롤러
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Author : 박병휘
  * @Date : 2024/05/13
  */
-@Controller
+@RestController
 public class LoadTestController {
+
     @GetMapping("/loaderio-204381adb0eab139a78a39d3acb01d6b/")
     public String LoadTest() {
-        return "index/loaderio-204381adb0eab139a78a39d3acb01d6b";
+        return "loaderio-204381adb0eab139a78a39d3acb01d6b<br>";
     }
 }
